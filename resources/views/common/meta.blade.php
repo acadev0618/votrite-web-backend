3f2dbfc31f661b55d0c0b2fb879a7b201d5f2e91
nikolay <nikolay82nikolvich@gmail.com>
2020-06-04 13:45:59 +0300

    changed
    changed

--- resources/views/common/meta.blade.php
+++ resources/views/common/meta.blade.php
@@ -8,5 +8,5 @@
 <meta content="" name="description"/>
 <meta content="" name="author"/>
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<meta name="baseurl" content="{{url('api/')}">
+<meta name="baseurl" content="{{url('api/')}}">
 
