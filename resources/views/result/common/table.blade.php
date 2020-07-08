ec341b4a6da49c8e45099786aa4c84a12c10b476
violetfocus <violetfocus0618@gmail.com>
2020-07-08 18:39:24 +0300

    table changed
    table changed

--- resources/views/result/common/table.blade.php
+++ resources/views/result/common/table.blade.php
@@ -1,18 +1,17 @@
-<h3 id="ballot_board">{{$ballots->data[0]->board}}</h3>
-<h3 >{{$ballots->data[0]->address}}</h3>
-<h3 >{{$ballots->data[0]->client}}</h3>
-<h3 >{{$ballots->data[0]->election}}</h3>
+<h4 style="font-weight: bold;" id="ballot_board">{{$ballots->data[0]->board}}</h4>
+<h4 style="font-weight: bold;" >{{$ballots->data[0]->address}}</h4>
+<h4 style="font-weight: bold;" >{{$ballots->data[0]->client}}</h4>
+<h4 style="font-weight: bold;" >{{$ballots->data[0]->election}}</h4>
 <br>
 @csrf
-<h2 class="text-center" style="border-bottom:1px solid">END OF DAY - REPORT</h2>
-<h2 >TOTAL VOTES ON MACHINE...........................{{$blt_cnt}}</h2>
-<hr style="border: 1px solid;width: 500px;">
+<h3 class="text-center" style="width: 400px;border-bottom:1px solid;font-weight: bold;">END OF DAY - REPORT</h3>
+<h5 style="font-weight: bold;" >TOTAL VOTES ON MACHINE........{{$blt_cnt}}</h5>
+<hr style="border: 1px solid;width: 400px;">
 <div id="countresult" class="form-group" style="margin-left:25px;">
 @if(count($candidates) != 0)
 @foreach($candidates as $race)
-    <h3>{{$race[0]->race_title}}</h3>
-    <br>
-    <h3>{{$race[0]->race_name}}</h4>
+    <h4 style="font-weight: bold;">{{$race[0]->race_title}}</h4>
+    <h4 style="font-weight: bold;">{{$race[0]->race_name}}</h4>
     @foreach($race as $cand)
     <h4>{{$cand->candidate_name}} - {{$cand->cast_counter}}</h4>
     @endforeach
@@ -21,13 +20,13 @@
 No Candidate
 @endif
 </div>
-<hr style="border: 1px solid;width: 500px;">
+<hr style="border: 1px solid;width: 400px;">
 <h2>Proposition</h2>
 <div id="propresult" class="form-group" style="margin-left:25px;">
 @if(count(get_object_vars($props)) != 0 && property_exists($props, "data"))
 @if(count($props->data) != 0)
 @foreach($props->data as $pro)
-    <h3>{{$pro->prop_name}}</h3>
+    <h4>{{$pro->prop_name}}</h4>
     @if($pro->prop_answer_type == 1)
     <h4>Yes - {{$pro->cast_yes}}</h4>
     <h4>No - {{$pro->cast_no}}</h4>
