2553b9b03d973d3d9c317043ebd17f0cec8883ad
violetfocus <violetfocus0618@gmail.com>
2020-07-08 17:41:49 +0300

    canvas added
    canvas added

--- resources/views/common/js.blade.php
+++ resources/views/common/js.blade.php
@@ -32,6 +32,7 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
+<script type="text/javascript" src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
 
 <script>
     window.apiurl = "{{env('APP_URL')}}/result/";
