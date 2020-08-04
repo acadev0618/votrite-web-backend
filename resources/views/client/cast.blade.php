d8f5873c63377d99dd262ab84041088f360a8e6b
violetfocus0618 <58705460+violetfocus0618@users.noreply.github.com>
2020-08-04 13:20:08 -0700

    change ui css and reduce settimer
    change ui css and reduce settimer

--- resources/views/client/cast.blade.php
+++ resources/views/client/cast.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-<div style="display: table; position: absolute; top: 0; left: 0; height: 100%; width: 100%; background:white;">
+<div style="display: table; position: absolute; top: 0; left: 0; height: 100%; width: 100%; background: #E0E5EC;">
     <div style="display: table-cell; vertical-align: middle;">
-      <div class="text-center" style="margin-left: auto; margin-right: auto; width: 600px;">
+      <div class="text-center" style="margin-left: auto; margin-right: auto; width: 100%;">
       	<img src="{{asset('assets/img/ic_finish.png')}}" style="color: white;" />
-        <h1 style="color: #7b6be2; font-size: 80px;">You are Voted</h1>
+        <h1 class="cast-text" style="color: #7b6be2; font-size: 50px;">You are Voted</h1>
       </div>
     </div>
 </div>
@@ -20,6 +20,6 @@ setTimeout(function(){
     window.location.href = "{{url('/')}}"; 
     window.location.href = "{{url('/')}}"; 
     window.location.href = "{{url('/')}}"; 
-}, 15000);
+}, 5000);
 </script>
 @endsection
